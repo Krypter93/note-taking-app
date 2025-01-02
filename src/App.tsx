1f082c47cabede32d32912f0af7d9818b82ecd83
Ahmed <ahmedfdez93@gmail.com>
2025-01-02 00:29:29 -0500

    Working on NotesView component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Sidebar from "./components/Sidebar"
 import AddModal from "./components/AddModal"
 import { RootState } from "./redux/store"
 import { useSelector } from "react-redux"
+import NotesView from "./components/NotesView"
 function App() {
   const addModalState = useSelector((state: RootState) => state.addModal.value)
 
@@ -9,6 +10,7 @@ function App() {
     <>
      <Sidebar />
      {addModalState && <AddModal />}
+     <NotesView />
     </>
   )
 }
