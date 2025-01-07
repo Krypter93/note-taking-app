19cd586df953d6ca82a7781d23b8e374ff8466ff
Ahmed <ahmedfdez93@gmail.com>
2025-01-07 15:10:30 -0500

    Fix UI issues of notes overflowing the viweport on the y axis, changes of gap and overflow in NotesView component and add fixed position to main div in Sidebar component

--- src/components/NotesView.tsx
+++ src/components/NotesView.tsx
@@ -37,7 +37,7 @@ export default function NotesView() {
     
   return (
      <>
-     <div className=" flex flex-row flex-wrap justify-center bg-violet-200 w-3/4 h-screen absolute top-[0%] left-[25%] p-10 gap-x-9 overflow-hidden">
+     <div className=" flex flex-row flex-wrap justify-center bg-violet-200 w-3/4 h-fit absolute top-[0%] left-[25%] p-10 gap-10">
       {notes.length > 0 && notes.map((note: NoteType ) =>(
           <div className= {`${note.color} w-[300px] h-[240px] rounded rounded-br-[50px] break-words whitespace-normal overflow-y-auto brightness-110`} key={note.id}>
             <div className={`p-0 m-0 w-full h-8 relative top-0 ${note.color} brightness-50`}></div>
@@ -48,14 +48,14 @@ export default function NotesView() {
             <div className="flex flex-row justify-center w-full">
             {editNoteId === note.id ? (
               <div className="flex flex-col">
-                <textarea className="w-[100%] p-2 rounded rounded-br-[30px] text-black outline-none" rows={4} value={updatedNote} onChange={onChangeNote}/>
+                <textarea className="w-[100%] p-2 rounded rounded-br-[30px] text-black outline-none" rows={4} cols={30} value={updatedNote} onChange={onChangeNote}/>
                 <div className="flex flex-row justify-around m-3">
                   <button className="bg-zinc-200 text-black pt-1 pb-1 pr-2 pl-2 rounded" onClick={() => handleModifyNote(note.id, updatedNote)}>Save</button>
                   <button className="bg-zinc-200 text-black pt-1 pb-1 pr-2 pl-2 rounded" onClick={handleCancelEditNote}>Cancel</button>
                 </div>
               </div>)
               :
-              (<p className="p-2 text-justify">{note.content}</p>)}
+              (<p className="p-3 text-justify text-lg">{note.content}</p>)}
             </div>
         </div>
       ))}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
   }
 
   return (
-    <div className='flex flex-col w-1/4 h-screen bg-violet-800'>
+    <div className='flex flex-col w-1/4 h-screen bg-violet-800 fixed'>
       <h1 className='p-2 mt-5 mx-auto text-white font-mono font-bold text-4xl'>Note Taking App</h1>
 
       <GiNotebook className='text-white mt-3 mx-auto font-bold text-9xl'/>
@@ -23,5 +23,7 @@ export default function Sidebar() {
         <p className='text-xl'> + Create New Note</p>
       </button>
     </div>
+
+    
   )
 }
